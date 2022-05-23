54cfe98d23bd288481987a6a518837327056ad17
Aman <amanaa.011@gmail.com>
2022-05-23 13:28:40 +0530

    commit

--- Number theory/01 check prime.py
+++ Number theory/01 check prime.py
@@ -1,9 +1,13 @@
 n=int(input())
 is_prime=True
-if n==1:
+if n<=1:
+    is_prime=False
+elif n==2:
+    is_prime=True
+elif n%2==0:
     is_prime=False
 else:
-    for i in range(2,int(n**0.5)+1):
+    for i in range(3, int(n**(1/2))+1, 2):
         if n%i==0:
             is_prime=False
             break
